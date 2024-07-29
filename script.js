bcd81a54b3cfb7228cf0aa8c3d727fe885b6c754
Neto lima <neto2805@hotmail.com>
2024-07-29 09:13:51 -0300

    commit dena cilios

---
+++ script.js
@@ -0,0 +1,18 @@
+
+const box = document.querySelector(".slideshow");
+const imagens = document.querySelectorAll(".slideshow img");
+
+let contador = 0;
+
+function slider() {
+    
+    contador ++;
+
+    if (contador > imagens.length -1){
+        contador = 0;
+    }
+
+    box.style.transform = `translateX(${-contador * 500}px})`
+}
+
+setInterval(slider, 2000);
