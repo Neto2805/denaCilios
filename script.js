7bafb696aa855fdc869c6bdf56af7dd256866704
Neto lima <neto2805@hotmail.com>
2024-08-20 20:15:36 -0300

    terceiro commit

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function slider() {
         contador = 0;
     }
 
-   box.style.transform = `translateX(${-contador * 500}px)`  //ALTERADO COM SUCESSOO
+   box.style.transform = `translateX(${-contador * 500}px)`  //ALTERADO COM SUCESSOOOOOO
 }
 
 setInterval(slider, 2000);
